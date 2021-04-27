fbc82bc71377ad685938506e0fa67b10080d870b
M Kusold <m@kusold.com>
2021-04-27 11:55:45 -0700

    relevant additions

--- setHyperPlugins.js
+++ setHyperPlugins.js
@@ -21,7 +21,7 @@ if(fs.existsSync(preferencePath)){
         moveRight: 'command+]'
     }`
     const settingsWithCustomPlugins = rawHyperPreferences.replace("// for advanced config flags please refer to https://hyper.is/#cfg", custumPluginSection);
-    const addPlugins = ['hyper-dracula', 'hyper-search', 'hypercwd', 'hyper-alt-click', 'hyper-pane']
+    const addPlugins = ['hyper-dracula', 'hyper-search', 'hypercwd', 'hyper-alt-click', 'hyper-pane', 'hyper-rename-tab']
     const newSettings = settingsWithCustomPlugins.replace(/plugins: \[\]/, "plugins: " + JSON.stringify(addPlugins) );
     fs.writeFile(preferencePath, newSettings, (e) => {if (e) throw e;});
 } else {
